b994a9723189fac92b991345acf60590dd11d58e
admin <admin@DESKTOP-14LTB1P>
2020-08-03 11:12:33 +0900

    Initial commit

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,25 @@
+/*
+	Date : 2020년 6월 26일
+	Author : 홍길동
+	Version : 1.8
+*/
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//주석입니다 코드를 설명 개발자가 보기위함
+		System.out.println("Hello,Java");
+		System.out.println("김근태");
+		/*
+		    여러줄 
+		    주석이다 
+		    이말이야
+		*/
+		
+		int year=2018;
+		System.out.println(year);
+	} 
+
+}
